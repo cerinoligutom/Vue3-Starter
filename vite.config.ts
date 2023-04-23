794ae840923b33b2edbec091d7a5327da7022ede
Cerino Ligutom III <zeferinix@gmail.com>
2023-04-24 01:17:52 +0800

    refactor: migrate fluent from rollup to unplugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import fluentPlugin from 'rollup-plugin-fluent-vue';
+import { SFCFluentPlugin } from 'unplugin-fluent-vue/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), fluentPlugin()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    SFCFluentPlugin({
+      // define messages in SFCs
+      blockType: 'fluent', // default 'fluent' - name of the block in SFCs
+      checkSyntax: true, // default true - whether to check syntax of the messages
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
