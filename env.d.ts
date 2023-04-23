08733a51cd6e561a81ae9dbe6f03266c9c8705fc
Cerino Ligutom III <zeferinix@gmail.com>
2023-04-24 01:07:43 +0800

    fix: missing types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+import type { App } from 'vue';
+import type { Router } from 'vue-router';
+
+declare global {
+  interface IModuleOptions {
+    app: App<Element>;
+    router: Router;
+  }
+
+  interface IModuleFn {
+    (moduleOptions: IModuleOptions): void;
+  }
+}
